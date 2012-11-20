9a8bbbd7220d75b75ac077953f91b56226be83cf
stephan.com <stephan@stephan.com>
2012-11-20 01:33:48 -0800

    first solution without formatting

--- spiralify.rb
+++ spiralify.rb
@@ -3,9 +3,76 @@
 n = ARGV.shift.to_i rescue 1 # TODO rescue better
 
 class Spiralify
+  # we describe the space of spiraled numbers as a hash of hashes
+  # indexed by their displacement from the start position.  
   class Field
+    DIRECTIONS = [:right, :up, :left, :down] # counterclockwise
+    INITIAL_DIRECTION = :right
+    NAVIGATION = {
+      right:  {x:  1, y:  0},
+      left:   {x: -1, y:  0},
+      up:     {x:  0, y:  1},
+      down:   {x:  0, y: -1}
+    }    
+    
+    attr_reader :manifold
+    
     def initialize(edge)
-      @the_board = [[nil]*edge]*edge # pre-intialize a 2D array to contain the whole thing
+      @manifold   = {} # store this space as a hash of hashes
+      @position   = {x: 0, y: 0} # current position
+      @direction  = :right
+    end
+    
+    def is_empty?(position)
+      @manifold[position[:x]][position[:y]].nil? rescue true
+    end
+    
+    def self.next_direction(direction)
+      DIRECTIONS[(DIRECTIONS.index(direction)+1) % DIRECTIONS.length]
+    end
+    
+    def next_position_for_direction(dir)
+      { x: @position[:x] + NAVIGATION[dir][:x],
+        y: @position[:y] + NAVIGATION[dir][:y] }
+    end
+    
+    def next_position
+      next_position_for_direction(@direction)
+    end
+    
+    def turn_position_is_empty?
+      is_empty?(next_position_for_direction(Field.next_direction(@direction)))
+    end
+    
+    def turn
+      @direction = Field.next_direction(@direction) if turn_position_is_empty?
+    end
+    
+    def to_a
+      @manifold.keys.sort.map { |y|
+        @manifold[y].keys.sort.map { |x|
+          @manifold[y][x]
+        }
+      }
+    end
+
+    def next_digit
+      turn if turn_position_is_empty?
+      @position = next_position
+    end
+    
+    def x
+      @position[:x]
+    end
+    def y
+      @position[:y]
+    end
+  
+    def add_digit(d)
+      # puts "hi #{d} #{x} #{y}"
+      @manifold[x] ||= {}
+      @manifold[x][y] = d
+      next_digit
     end
   end
   
@@ -13,12 +80,19 @@ class Spiralify
     @n = n
   end
   
+  
   def spiralize
-    [@n]
+    f = Field.new(Math.sqrt(@n))
+    (1..@n).each do |i|
+      f.add_digit(i)
+    end
+    f.to_a
+    # f.manifold
   end
   
   def print_spiral
     puts "I should be outputting a spiral up to #{@n}"
+    puts spiralize.inspect
   end
 end
 
--- spiralify_spec.rb
+++ spiralify_spec.rb
@@ -1,53 +1,72 @@
 require './spiralify'
 
 describe Spiralify, '#spiralize' do
-  shared_examples_for 'a square' do
-    subject { Spiralify.new(n).spiralize }
-    it "returns the correct result" do
-      should eq(result)
-    end
+  describe "spirals" do    
+    shared_examples_for 'a square' do
+      subject { Spiralify.new(n).spiralize }
+      it "returns the correct result" do
+        should eq(result)
+      end
     
-    it "includes 1" do
-      subject.flatten.should include(1)
-    end
+      it "includes 1" do
+        subject.flatten.should include(1)
+      end
     
-    it "includes n" do
-      subject.flatten.should include(n)
-    end
+      it "includes n" do
+        subject.flatten.should include(n)
+      end
     
-    it "does not include 0" do
-      subject.flatten.should_not include(0)
+      it "does not include 0" do
+        subject.flatten.should_not include(0)
+      end
+    end
+  
+    describe "n=1" do
+      let(:n) { 1 }
+      let(:result) { [[1]] }
+
+      it_behaves_like 'a square'
+    end
+    describe "n=4" do
+      let(:n) { 4 }
+      let(:result) { [[4, 3],
+                      [1, 2]] }
+
+      it_behaves_like 'a square'
+    end
+    describe "n=9" do
+      let(:n) { 9 }
+      let(:result) { [[5, 4, 3],
+                      [6, 1, 2],
+                      [7, 8, 9]] }
+
+      it_behaves_like 'a square'
+    end
+    describe "n=16" do
+      let(:n) { 16 }
+      let(:result) { [[16, 15, 14, 13],
+                      [ 5,  4,  3, 12],
+                      [ 6,  1,  2, 11],
+                      [ 7,  8,  9, 10]] }
+
+      it_behaves_like 'a square'
     end
   end
   
-  describe "n=1" do
-    let(:n) { 1 }
-    let(:result) { [1] }
-
-    it_behaves_like 'a square'
-  end
-  describe "n=4" do
-    let(:n) { 4 }
-    let(:result) { [[4, 3],
-                    [1, 2]] }
-
-    it_behaves_like 'a square'
-  end
-  describe "n=9" do
-    let(:n) { 9 }
-    let(:result) { [[5, 4, 3],
-                    [6, 1, 2],
-                    [7, 8, 9]] }
-
-    it_behaves_like 'a square'
-  end
-  describe "n=16" do
-    let(:n) { 16 }
-    let(:result) { [[16, 15, 14, 13],
-                    [ 5,  4,  3, 12],
-                    [ 6,  1,  2, 11],
-                    [ 7,  8,  9, 10]] }
-
-    it_behaves_like 'a square'
+  describe "field directions" do
+    subject { Spiralify::Field }
+    
+    it "should go right to up" do
+      subject.next_direction(:right).should eq(:up)
+    end
+    it "should go up to left" do
+      subject.next_direction(:up).should eq(:left)
+    end
+    it "should go left to down" do
+      subject.next_direction(:left).should eq(:down)
+    end
+    it "should go down to right" do
+      subject.next_direction(:down).should eq(:right)
+    end
   end
 end
